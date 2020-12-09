e7ce2e4d4f881153d49ccc85d41aba0a4fcaae7b
Tessakam <tessa.kam@hotmail.com>
2020-12-09 17:03:03 +0100

    all requirements done but still have some errors to solve

--- friends/src/app/app.component.ts
+++ friends/src/app/app.component.ts
@@ -1,6 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Friend} from './friend';
 import {AddFriendService} from './add-friend.service'
+import {HttpClient} from '@angular/common/http';
+import {HttpHeaders} from "@angular/common/http";
 
 
 @Component({
@@ -9,24 +11,40 @@ import {AddFriendService} from './add-friend.service'
   styleUrls: ['./app.component.css']
 })
 
-
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "Hello friends";
   listLanguages = ["HTML", "CSS", "JS", "PHP"];
   friendModel = new Friend("", "", "", "", "")
-  public AddFriendService: any;
-  allFriends: any= [];
+  private AddFriendService: any;
+  public allFriends: any = [];
 
-  constructor(AddFriendService: AddFriendService) {
+
+  constructor(AddFriendService: any, http: HttpClient) {
     this.AddFriendService = AddFriendService;
   }
 
-  addFriend() : void {
-    this.AddFriendService.addFriend(this.friendModel).subscribe((data: Friend ) => console.log(data))
+  addFriend(): void {
+    this.AddFriendService.addFriend(this.friendModel).subscribe((data: Friend) => this.getAllFriends('http://localhost:9000/allFriends'));
     console.log(this.friendModel);
-    }
   }
 
+  // display updated friend list
+  async getAllFriends(url: string): Promise<any> {
 
+    const response = await fetch(url, {
+      //The method should be get and the headers should be 'Content-Type': 'application/json'.
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
 
+    const data = await response.json()
+    this.allFriends = data;
+    return data;
+  }
 
+  ngOnInit():any{
+    this.getAllFriends('http://localhost:9000/allFriends')
+  }
+}
