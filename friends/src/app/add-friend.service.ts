18f70385b5b687bbec95d264fba1946738f71e1c
Tessakam <tessa.kam@hotmail.com>
2020-12-09 09:56:15 +0100

    added method addFriend

---
+++ friends/src/app/add-friend.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { Friend } from "./friend";
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AddFriendService {
+
+  constructor() { }
+
+  addFriend(friend: Friend){
+    return this.http.post(url, "temp")
+  }
+}
--- friends/src/app/app.component.ts
+++ friends/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import {Friend} from "./friend";
+import {Component} from '@angular/core';
+import {Friend} from './friend';
+import {AddFriendService} from './add-friend.service'
 
 
 @Component({
@@ -8,12 +9,24 @@ import {Friend} from "./friend";
   styleUrls: ['./app.component.css']
 })
 
+
 export class AppComponent {
   title = "Hello friends";
   listLanguages = ["HTML", "CSS", "JS", "PHP"];
-  friendModel = new Friend ("","","","","")
+  friendModel = new Friend("", "", "", "", "")
+  private AddFriendService: any;
 
-}
+  constructor(AddFriendService: AddFriendService) {
+  }
+
+  //In the method, call the addFriend method of the addFriendService and pass the friend data to it.
+  //"observable" = returns method and to get the data out,use subscribe
+  addFriend() {
+    this.AddFriendService.addFriend(this.friendModel).subscribe(data => console.log(data))
+      //console.log(this.friendModel);
+    }
+  }
+
 
 
 
